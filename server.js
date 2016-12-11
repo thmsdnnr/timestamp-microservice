408cfc1218fd718e522d0998ff63a97e4c763459
thomas danner <thmsdnnr@gmail.com>
2016-12-11 23:32:04 +0000

    changes for heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var util=require('util');
 var moment=require('moment');
 var path=require('path');
 var app=express();
-//app.use(express.static('/'));
+app.use(express.static('/'));
 app.get('/:date',function(req,res) {
 var inDate=(JSON.stringify(req.params.date));
 inDate=inDate.slice(1,inDate.length-1); //slice strips quotes from beginning and end
@@ -19,5 +19,5 @@ else { res.send({"unix":day.unix(),"natural":day.format("MMMM D, YYYY")}); res.e
 }
 function invalidQuit () {res.send({"unix":null,"natural":null}); res.end();}
 });
-//app.get('*',function(req,res){res.sendFile(path.join(__dirname, 'index.html'));});
+app.get('*',function(req,res){res.sendFile(path.join(__dirname, 'index.html'));});
 app.listen(process.env.PORT||8080,function() {console.log('listening on port '+port); });
