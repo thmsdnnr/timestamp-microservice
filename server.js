073a97dacfc570953e2582b031ac753c618fa580
thomas danner <thmsdnnr@gmail.com>
2016-12-11 18:42:23 +0000

    first working commit hooray

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 var express=require('express');
-var app=express()
-
-app.get('/',function(req,res) {
-res.send('Hello World');
-})
+var util=require('util');
+var moment=require('moment');
+var path=require('path');
+var app=express();
+app.use(express.static('/'));
+app.get('/:date',function(req,res) {
+var inDate=(JSON.stringify(req.params.date));
+inDate=inDate.slice(1,inDate.length-1); //slice strips quotes from beginning and end
+if (!inDate.match(/[A-Z][a-z]*/g)) { 
+//possible unix timestamp, no natural language
+var day=moment.unix(inDate); if (day.isValid()===false) {invalidQuit();}
+//calculate natural language date off timestamp
+else{res.send({"unix":day.unix(),"natural":day.format("MMMM D, YYYY")}); res.end(); }
+}
+else { //some natural language
+var day=moment(inDate); if (day.isValid()===false) {invalidQuit();}
+else { res.send({"unix":day.unix(),"natural":day.format("MMMM D, YYYY")}); res.end(); }
+}
+function invalidQuit () {res.send({"unix":null,"natural":null}); res.end();}
+});
+app.get('*',function(req,res){res.sendFile(path.join(__dirname, 'index.html'));});
 
 app.listen(8080,function() {console.log('listening on port 8080'); });
